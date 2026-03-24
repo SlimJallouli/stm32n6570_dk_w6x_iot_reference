285f7d9936aa3451c9ba25405b34e03d61e6f30f
SlimJallouli <slim.jallouli@st.com>
2026-03-24 12:14:36 -0700

    Fix issue with memcpy

--- Appli/Core/Src/crypto/hardware_rng.c
+++ Appli/Core/Src/crypto/hardware_rng.c
@@ -56,20 +56,27 @@ UBaseType_t uxRand(void)
 }
 #endif
 
-#if defined( MBEDTLS_ENTROPY_HARDWARE_ALT )
-int mbedtls_hardware_poll( void *Data, unsigned char *Output, size_t Len, size_t *oLen )
+#if defined(MBEDTLS_ENTROPY_HARDWARE_ALT)
+int mbedtls_hardware_poll(void *Data,
+                          unsigned char *Output,
+                          size_t Len,
+                          size_t *oLen)
 {
-  uint32_t index;
-  uint32_t randomValue;
+    size_t produced = 0;
 
-  for (index = 0; index < Len/4; index++)
-  {
-    randomValue = uxRand();
+    while (produced < Len)
+    {
+        uint32_t rnd = uxRand();
 
-    *oLen += 4;
-    memset(&(Output[index * 4]), (int)randomValue, 4);
-  }
+        /* Write up to 4 bytes from rnd into Output */
+        for (size_t i = 0; i < 4 && produced < Len; i++)
+        {
+            Output[produced++] = (unsigned char)(rnd >> (i * 8));
+        }
+    }
 
-  return 0;
+    *oLen = produced;
+    return 0;
 }
 #endif
+
